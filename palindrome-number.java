f3c248f4a55afb41c0b49a661cb3d21b48abb18c
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-08-13 18:22:10 +0530

    13/08

---
+++ palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str=Integer.toString(x);
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i++)!=str.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+}
